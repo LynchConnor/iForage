43468b99b50dc22397f001f2b3f539a138b5f3bf
CloseBuyUK <86831782+CloseBuyUK@users.noreply.github.com>
2021-10-06 22:42:25 +0100

    Initial Commit

---
+++ ForageNearMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ForageNearMe
+//
+//  Created by Connor A Lynch on 06/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ForageNearMe/ForageNearMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForageNearMeApp.swift
+//  ForageNearMe
+//
+//  Created by Connor A Lynch on 06/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct ForageNearMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
